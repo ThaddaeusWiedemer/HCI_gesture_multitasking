9a5d82ccc6479ebd115df789a7941fe5f2a261b7
ThaddaeusWiedemer <thaddaeus.wiedemer@gmail.com>
2019-05-14 16:00:25 +0800

    test gesturedetector

--- app/src/main/java/com/gesturemultitasking/MainActivity.java
+++ app/src/main/java/com/gesturemultitasking/MainActivity.java
@@ -1,13 +1,52 @@
 package com.gesturemultitasking;
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
+    private GestureDetectorCompat mDetector;
+    private TextView tv = findViewById(R.id.gesture_output);
+
+    // Called when the activity is first created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d(DEBUG_TAG,"onDown: " + event.toString());
+            tv.setText("onDown");
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+            tv.setText("onFling");
+            return true;
+        }
+    }
+
 }
