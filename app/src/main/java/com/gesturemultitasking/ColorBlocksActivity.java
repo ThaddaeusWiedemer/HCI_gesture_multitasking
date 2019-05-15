a71463b73f8fb38c1033edc8b76002a010c94dc8
ThaddaeusWiedemer <thaddaeus.wiedemer@gmail.com>
2019-05-15 13:36:05 +0800

    * included gestures in colorBlockActivity * using immersive_sticky mode to hide navigation and status bar and allow swipes through it * added debug-toasts

--- app/src/main/java/com/gesturemultitasking/ColorBlocksActivity.java
+++ app/src/main/java/com/gesturemultitasking/ColorBlocksActivity.java
@@ -9,15 +9,14 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ColorBlocksActivity extends AppCompatActivity {
 
     private ScaleGestureDetector mScaleGestureDetector;
     private SwipeGestureDetector mMoveGestureDetector;
-    private String name = "";
-    private String position = "";
-    private String options = "";
-    private TextView g_n, g_p, g_o;
+    private String text = "";
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,21 +41,23 @@ public class ColorBlocksActivity extends AppCompatActivity {
         mScaleGestureDetector = new ScaleGestureDetector(this, new PinchListener());
         mMoveGestureDetector = new SwipeGestureDetector(this, new SwipeListener());
 
-        // debug text views
-        g_n = findViewById(R.id.g_n);
-        g_p = findViewById(R.id.g_p);
-        g_o = findViewById(R.id.g_o);
+        // debug toasts
+        Context context = getApplicationContext();
+        toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_SHORT);
     }
 
     // handels all touch events and calls the gesture listeners
     @Override
     public boolean onTouchEvent(MotionEvent event){
+        text = "";
         mMoveGestureDetector.onTouchEvent(event);
         mScaleGestureDetector.onTouchEvent(event);
-        // output debug messages
-        g_n.setText(name);
-        g_p.setText(position);
-        g_o.setText(options);
+        if(text != "") {
+            toast.cancel();
+            toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+            toast.show();
+        }
         return true;
     }
 
@@ -64,9 +65,9 @@ public class ColorBlocksActivity extends AppCompatActivity {
     private class PinchListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
-            name = "Pinch";
-            position = "Focus " + detector.getFocusX() + " " + detector.getFocusY();
-            options = "Span " + detector.getCurrentSpanX() + " " + detector.getCurrentSpanY();
+            text = "Pinch\n" +
+                "Focus " + detector.getFocusX() + " " + detector.getFocusY() + "\n" +
+                "Span " + detector.getCurrentSpanX() + " " + detector.getCurrentSpanY();
             return true;
         }
     }
@@ -75,23 +76,23 @@ public class ColorBlocksActivity extends AppCompatActivity {
     private class SwipeListener extends SwipeGestureDetector.SimpleOnSwipeGestureListener {
         @Override
         public boolean onSwipe(SwipeGestureDetector detector) {
-            name = "Swipe";
-            position = "Start " + detector.getInitialFocus().x + " " + detector.getInitialFocus().y;
-            options = "Delta " + detector.getFocusX() + " " + detector.getFocusY();
+            text = "Swipe\n" +
+                "Start " + detector.getInitialFocus().x + " " + detector.getInitialFocus().y + "\n" +
+                "Delta " + detector.getFocusX() + " " + detector.getFocusY();
             return true;
         }
 
         public boolean onOutSwipe(SwipeGestureDetector detector) {
-            name = "OutSwipe";
-            position = "Start " + detector.getInitialFocus().x + " " + detector.getInitialFocus().y;
-            options = "Delta " + detector.getFocusX() + " " + detector.getFocusY();
+            text = "OutSwipe\n" +
+                "Start " + detector.getInitialFocus().x + " " + detector.getInitialFocus().y + "\n" +
+                "Delta " + detector.getFocusX() + " " + detector.getFocusY();
             return true;
         }
 
         public boolean onInSwipe(SwipeGestureDetector detector) {
-            name = "InSwipe";
-            position = "Start " + detector.getInitialFocus().x + " " + detector.getInitialFocus().y;
-            options = "Delta " + detector.getFocusX() + " " + detector.getFocusY();
+            text = "InSwipe\n" +
+                "Start " + detector.getInitialFocus().x + " " + detector.getInitialFocus().y + "\n" +
+                "Delta " + detector.getFocusX() + " " + detector.getFocusY();
             return true;
         }
     }
